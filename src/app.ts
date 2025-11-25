9c1e445fe75bbf7ab7ad55dc104530b535abfd53
Jorge Bublitz <96549762+JorgeBublitz@users.noreply.github.com>
2025-11-24 23:18:25 -0300

    Update CORS origin to new URL

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "https://fhub-api.vercel.app/",
+  origin: "https://fhub.vercel.app",
   credentials: true
 }));
 
